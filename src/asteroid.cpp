a62d4937588c25e367c980c69a9451c78e02df8e
Samantha Doran <Vixen.Miyuki@gmail.com>
2013-05-05 00:01:29 -0400

    Make asteroids have the same behaviour as the player, the y should loop around the screen

--- src/asteroid.cpp
+++ src/asteroid.cpp
@@ -15,7 +15,56 @@ void Asteroid::update(sf::Time time)
 {
     x+=velocity.getXComponent()*time.asSeconds();
     y+=velocity.getYComponent()*time.asSeconds();
+
+
+    bool allAbove  = true;
+    bool allBelow  = true;
+    bool allLeft   = true;
+    bool allRight  = true;
+    //If any exceptions, the conditional goes false
+    //for(unsigned int i = 0; i < circle.getPointCount(); ++i)
+    //{
+    if(circle.getRadius() + circle.getPosition().y > -1*circle.getRadius()*4)
+    {
+        allAbove = false;
+    }
+    if(circle.getRadius() + circle.getPosition().y < 1030 + circle.getRadius()*2.5*2)
+    {
+        allBelow = false;
+    }
+    if(circle.getRadius() + circle.getPosition().x > -1*circle.getRadius()*4)
+    {
+        allLeft = false;
+    }
+    if(circle.getRadius() + circle.getPosition().x < 1920 + circle.getRadius()*2.5*2)
+    {
+        allRight = false;
+    }
+
+    //Loop around, asteroids style.
+    if(allAbove)
+    {
+        y = 1030+circle.getRadius();
+        circle.setPosition(circle.getPosition().x, y);
+    }
+    if(allBelow)
+    {
+        y = -2*circle.getRadius();
+        circle.setPosition(circle.getPosition().x, y);
+    }
+    if(allLeft)
+    {
+        x = 1920 + circle.getRadius();
+        circle.setPosition(x, circle.getPosition().y);
+    }
+    if(allRight)
+    {
+        x = -2*circle.getRadius();
+        circle.setPosition(x, circle.getPosition().y);
+    }
+
     circle.setPosition(x, y);
+
 }
 
 sf::CircleShape Asteroid::getCircle() const
